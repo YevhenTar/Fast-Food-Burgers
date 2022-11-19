669120d0388f731528c34f8b2ba18d9902550a66
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-11-19 17:59:06 +0200

    Add html plugin to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        main: './js/script.js'
+        // ['scroll-out']: './src/js/scroll-out.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            // favicon: './src/assets/img/favicon/favicon-16x16.png',
+            scriptLoading: 'blocking'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
